f921870e98c798267f2100038dce56b5da79d379
Yassine Zaazaa <yzaazaa@e1r8p5.1337.ma>
2024-05-06 20:00:58 +0100

    Deleted unused condition

--- srcs/raycasting/player_moves.c
+++ srcs/raycasting/player_moves.c
@@ -6,7 +6,7 @@
 /*   By: Razog <yassine.zaaaza@outlook.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 01:09:07 by frukundo          #+#    #+#             */
-/*   Updated: 2024/05/06 19:55:52 by Razog            ###   ########.fr       */
+/*   Updated: 2024/05/06 20:00:30 by Razog            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	move_forword(t_datas *game)
 {
 	if (game->map->map2d[(int)(game->pos_x + game->x_dir)][(int)(\
-		game->pos_y + game->y_dir)] == '0' || game->map->map2d[(int)(game->pos_x + game->x_dir)][(int)(game->pos_y + game->y_dir)] == ' ')
+		game->pos_y + game->y_dir)] == '0')
 	{
 		game->pos_x += game->x_dir * game->movespeed;
 		game->pos_y += game->y_dir * game->movespeed;
@@ -25,8 +25,7 @@ void	move_forword(t_datas *game)
 void	move_backword(t_datas *game)
 {
 	if (game->map->map2d[(int)(game->pos_x - game->x_dir)][(int)(\
-		game->pos_y - game->y_dir)] == '0' || game->map->map2d[(int)(game->pos_x - game->x_dir)][(int)(\
-		game->pos_y - game->y_dir)] == ' ')
+		game->pos_y - game->y_dir)] == '0')
 	{
 		game->pos_x -= game->x_dir * game->movespeed;
 		game->pos_y -= game->y_dir * game->movespeed;
@@ -36,8 +35,7 @@ void	move_backword(t_datas *game)
 void	move_left(t_datas *game)
 {
 	if (game->map->map2d[(int)(game->pos_x)][(int)(\
-		game->pos_y + game->y_dir - game->y_plane)] == '0' || game->map->map2d[(int)(game->pos_x)][(int)(\
-		game->pos_y + game->y_dir - game->y_plane)] == ' ')
+		game->pos_y + game->y_dir - game->y_plane)] == '0')
 	{
 		game->pos_y -= game->y_plane * game->movespeed;
 		game->pos_x -= game->x_plane * game->movespeed;
@@ -47,8 +45,7 @@ void	move_left(t_datas *game)
 void	move_right(t_datas *game)
 {
 	if (game->map->map2d[(int)(game->pos_x)][(int)(\
-		game->pos_y + game->y_dir + game->y_plane)] == '0' || game->map->map2d[(int)(game->pos_x)][(int)(\
-		game->pos_y + game->y_dir + game->y_plane)] == ' ')
+		game->pos_y + game->y_dir + game->y_plane)] == '0')
 	{
 		game->pos_y += game->y_plane * game->movespeed;
 		game->pos_x += game->x_plane * game->movespeed;
