a7da692a2347f2ab2bcdf1f5e42df035c518e1b7
Yassine Zaazaa <yzaazaa@e2r4p3.1337.ma>
2024-05-16 14:22:50 +0100

    Minimap functioning

--- srcs/mini_map.c
+++ srcs/mini_map.c
@@ -28,32 +28,35 @@ void	draw_mini_map(t_datas *game)
 
 	offset_x = game->screen_w / 2 - ((MINI_MAP_SIZE / 2) * TILE_SIZE);
 	player.x = floor(game->pos_x) - (MINI_MAP_SIZE / 2);
-	if (player.x < 0)
-		player.x = 0;
 	if (player.x + MINI_MAP_SIZE > game->map->row)
 		player.x = game->map->row - MINI_MAP_SIZE;
+	if (player.x < 0)
+		player.x = 0;
 	player.y = floor(game->pos_y) - (MINI_MAP_SIZE / 2);
-	if (player.y < 0)
-		player.y = 0;
 	if (player.y + MINI_MAP_SIZE >  game->map->col)
 		player.y = game->map->col - MINI_MAP_SIZE;
+	if (player.y < 0)
+		player.y = 0;
+	print_array(game->map->map2d);
+	printf("initial position: %d, %d\n", (int)floor(game->pos_x), (int)floor(game->pos_y));
+	printf("changed position: %d, %d\n", player.x, player.y);
 	starting_pixel.x = offset_x;
 	starting_pixel.y = 0;
 	end_pixel.x = offset_x + (MINI_MAP_SIZE * TILE_SIZE);
 	end_pixel.y = MINI_MAP_SIZE * TILE_SIZE;
-	while (starting_pixel.x < end_pixel.x)
+	while (starting_pixel.x < end_pixel.x && game->map->map2d[player.x])
 	{
 		starting_pixel.y = 0;
 		player.y = floor(game->pos_y) - (MINI_MAP_SIZE / 2);
-		if (player.y < 0)
-			player.y = 0;
 		if (player.y + MINI_MAP_SIZE >  game->map->col)
 			player.y = game->map->col - MINI_MAP_SIZE;
-		while (starting_pixel.y < end_pixel.y)
+		if (player.y < 0)
+			player.y = 0;
+		while (starting_pixel.y < end_pixel.y && game->map->map2d[player.x][player.y])
 		{
 			if (player.y == floor(game->pos_y) && player.x == floor(game->pos_x))
 				color = 0xFF0000;
-			else if (game->map->map2d[player.x][player.y] == '1')
+			else if ((printf("%d, %d\n", player.x, player.y)), game->map->map2d[player.x][player.y] == '1')
 				color = 0x000000;
 			else
 				color = 0xFFFFFF;
